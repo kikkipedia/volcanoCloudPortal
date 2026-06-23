0340acd6f4b29a92bf7cfd74da67d5d17840ebeb
Santiago Arellano <49238833+sarellanos@users.noreply.github.com>
2026-06-23 11:45:46 +0200

    Update eruption regimes and features for clarity

--- model/src/constants.js
+++ model/src/constants.js
@@ -1,5 +1,5 @@
 export const eruptiveRegimes = [
-    {depth: 2.5, gas: 0, h0: 5.9, h10: 5.7, h20: 5.4, regime: "weak"},
+    {depth: 2.5, gas: 0, h0: 5.9, h10: 5.7, h20: 5.4, regime: "no eruption"},
     {depth: 2.5, gas: 1, h0: 6.1, h10: 5.9, h20: 5.6, regime: "weak"},
     {depth: 2.5, gas: 2, h0: 6.4, h10: 6.1, h20: 5.8, regime: "weak"},
     {depth: 2.5, gas: 3, h0: 6.7, h10: 6.3, h20: 6.0, regime: "transitional"},
@@ -7,7 +7,7 @@ export const eruptiveRegimes = [
     {depth: 2.5, gas: 5, h0: 7.3, h10: 6.9, h20: 6.6, regime: "plinian"},
     {depth: 2.5, gas: 6, h0: 7.6, h10: 7.2, h20: 6.8, regime: "plinian"},
 
-    {depth: 5.0, gas: 0, h0: 6.2, h10: 5.9, h20: 5.6, regime: "weak"},
+    {depth: 5.0, gas: 0, h0: 6.2, h10: 5.9, h20: 5.6, regime: "no eruption"},
     {depth: 5.0, gas: 1, h0: 6.6, h10: 6.2, h20: 5.9, regime: "weak"},
     {depth: 5.0, gas: 2, h0: 6.9, h10: 6.5, h20: 6.2, regime: "transitional"},
     {depth: 5.0, gas: 3, h0: 7.3, h10: 6.9, h20: 6.6, regime: "transitional"},
@@ -15,7 +15,7 @@ export const eruptiveRegimes = [
     {depth: 5.0, gas: 5, h0: 8.0, h10: 7.6, h20: 7.3, regime: "plinian"},
     {depth: 5.0, gas: 6, h0: 8.4, h10: 8.0, h20: 7.6, regime: "plinian"},
 
-    {depth: 7.5, gas: 0, h0: 6.5, h10: 6.1, h20: 5.8, regime: "weak"},
+    {depth: 7.5, gas: 0, h0: 6.5, h10: 6.1, h20: 5.8, regime: "no eruption"},
     {depth: 7.5, gas: 1, h0: 6.9, h10: 6.5, h20: 6.2, regime: "transitional"},
     {depth: 7.5, gas: 2, h0: 7.3, h10: 6.9, h20: 6.6, regime: "transitional"},
     {depth: 7.5, gas: 3, h0: 7.7, h10: 7.3, h20: 7.0, regime: "plinian"},
@@ -23,7 +23,7 @@ export const eruptiveRegimes = [
     {depth: 7.5, gas: 5, h0: 8.5, h10: 8.1, h20: 7.7, regime: "plinian"},
     {depth: 7.5, gas: 6, h0: 8.9, h10: 8.5, h20: 8.1, regime: "plinian"},
 
-    {depth: 10, gas: 0, h0: 6.8, h10: 6.4, h20: 6.0, regime: "weak"},
+    {depth: 10, gas: 0, h0: 6.8, h10: 6.4, h20: 6.0, regime: "no eruption"},
     {depth: 10, gas: 1, h0: 7.2, h10: 6.8, h20: 6.5, regime: "transitional"},
     {depth: 10, gas: 2, h0: 7.7, h10: 7.3, h20: 7.0, regime: "transitional"},
     {depth: 10, gas: 3, h0: 8.1, h10: 7.7, h20: 7.3, regime: "plinian"},
@@ -33,14 +33,24 @@ export const eruptiveRegimes = [
 ];
 
 export const eruptionFeatures = {
+    "no eruption": {
+        smoke: "none",
+        ashAmount: "none",
+        sound: "silence",
+        infoBoxText: `
+            <h2>No Eruption</h2>
+            If the magma would contain no volatiles (H2O, CO2, SO2, HCl, HF, etc.),
+            no eruptive activity will occur.
+        `
+    },
     weak: {
         smoke: "light",
         ashAmount: "none",
         sound: "silence",
         infoBoxText: `
             <h2>Weak Eruption</h2>
-            Lorem ipsum dolor sit amet consectetur adipiscing elit.
-            Quisque faucibus ex sapien vitae pellentesque sem placerat.
+            If enough gas can escape from the magma avoiding overpressure, 
+            only weak eruptive or passive degassing activity will occurr.
         `
     },
     transitional: {
@@ -51,8 +61,9 @@ export const eruptionFeatures = {
         sound: "mild_eruption_sfx",
         infoBoxText: `
             <h2>Transitional Eruption</h2>
-            In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
-            Pulvinar vivamus fringilla lacus nec metus bibendum egestas.
+            When gas reaches the surface with a pressure slightly above ambient,
+            some explosive activity is expected, probably including the emission of
+            fragmented magma (ash).
         `
     },
     plinian: {
@@ -63,11 +74,11 @@ export const eruptionFeatures = {
         sound: "strong_eruption_sfx",
         infoBoxText: `
             <h2>Plinian Eruption</h2>
-            <p>
-                Iaculis massa nisl malesuada lacinia integer nunc posuere.
-                Ut hendrerit semper vel class aptent taciti sociosqu.
-            </p>
-            <p>Ad litora torquent per conubia nostra inceptos himenaeos.</p>
+            Gas-rich magma ascending quickly through the conduit will most likely create
+            significant overpressure when reaching the surface. This leads to explosive
+            volcanic eruption, with fragmented magma (ash).
+
+            If the volcanic column collapses, a pyroclastic flow will be produced.
         `
     }
 };
